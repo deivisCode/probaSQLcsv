9439abb40c2ab662baa4df031ee28587c249eac7
deivis <david.cotelo.online+github@gmail.com>
2025-11-14 01:25:09 +0100

    primeiro esquema usable

--- ordes.sql
+++ ordes.sql
@@ -1,40 +1,57 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS Definicions ;
+DROP TABLE IF EXISTS Termos ;
+
 BEGIN TRANSACTION;
 
-    CREATE TABLE IF NOT EXISTS Termos(
-        id_termos INTEGER PRIMARY KEY AUTOINCREMENT,
-        gl TEXT NOT NULL UNIQUE,
-        en TEXT NOT NULL,
-        es TEXT NOT NULL,
-        area_tematica_1 TEXT NOT NULL,
-        area_tematica_2 TEXT NOT NULL,
-        area_tematica_3 TEXT NOT NULL,
-        areas TEXT GENERATE ALWAYS AS ( concat_ws (' > ',area_tematica_1,area_tematica_2,area_tematica_3)) STORED,
-        xenero TEXT NOT NULL CHECK (xenero in ('femenino', 'masculino', 'neutro') ),
-        clase TEXT NOT NULL CHECK (clase in ('sustantivo', 'verbo', 'adxetivo')),
-        numeros TEXT NOT NULL CHECK (numeros in ('singular', 'plural')),
-        abreviacion TEXT NOT NULL CHECK (abreviacion in ('forma completa', 'siglas', 'abreviacion')),
-        fontes TEXT NOT NULL,
-        -- https://sqlite.org/datatype3.html
-        -- https://sqlite.org/lang_datefunc.html
-        -- data_inclusion
-        -- data_modificacion
-        FOREIGN KEY (xenero) REFERENCES Denominacions (proba)
+    CREATE TABLE Termos(
+        id          INTEGER PRIMARY KEY,
+        gl          TEXT NOT NULL UNIQUE, -- tal vez debería quitar a ligadura de 'unique' aqui e usar esto como clave foránea mellor
+        en          TEXT NOT NULL ,
+        es          TEXT NOT NULL ,
+        xenero      TEXT NOT NULL CHECK ( xenero      in ( 'femenino'       , 'masculino' , 'neutro'      ) ) ,
+        clase       TEXT NOT NULL CHECK ( clase       in ( 'sustantivo'     , 'verbo'     , 'adxetivo'    ) ) ,
+        numeros     TEXT NOT NULL CHECK ( numeros     in ( 'singular'       , 'plural'                    ) ) ,
+        abreviacion TEXT NOT NULL CHECK ( abreviacion in ( 'forma completa' , 'siglas'    , 'abreviación' ) )
     );
 
-END TRANSACTION;
-
-
-BEGIN TRANSACTION;
-
-    CREATE TABLE IF NOT EXISTS Denominacions(
-        id_denominacions INTEGER PRIMARY KEY AUTOINCREMENT,
-        proba TEXT
+    CREATE TABLE Definicions(
+        id            INTEGER PRIMARY KEY,
+        id_definicion INTEGER NOT NULL,
+        definicion    TEXT    NOT NULL,
+        FOREIGN KEY (id_definicion) REFERENCES Termos(id)
     );
 
-END TRANSACTION;
+    CREATE TABLE IF NOT EXISTS AreasTematicas(
+        id      INTEGER PRIMARY KEY ,
+        id_area INTEGER NOT NULL ,
+        area    TEXT    NOT NULL CHECK ( area in ( "area_1", "area_2", "area_3", "area_4" )),
+        FOREIGN KEY (id_area) REFERENCES Termos(id)
+    );
 
-BEGIN TRANSACTION;
-    INSERT INTO Denominacions(proba) VALUES ('femenino', 'masculino', 'neutro');
+    INSERT INTO
+        Termos( gl, en, es, xenero, clase, numeros, abreviacion )
+    VALUES
+        ( "termo_gl_1", "termo_en_1", "termo_es_1", "femenino", "adxetivo", "plural", "forma completa"),
+        ( "termo_gl_2", "termo_en_2", "termo_es_2", "femenino", "adxetivo", "plural", "forma completa"),
+        ( "termo_gl_3", "termo_en_3", "termo_es_3", "femenino", "adxetivo", "plural", "forma completa");
+
+    INSERT INTO
+        AreasTematicas( id_area, area )
+    VALUES
+        (1, "area_1"),
+        (1, "area_2"),
+        (1, "area_3"),
+        (2, "area_2");
+
+    INSERT INTO
+        Definicions( id_definicion, definicion )
+    VALUES
+        (1, "definicion_1_1"),
+        (1, "definicion_1_2"),
+        (1, "definicion_1_3"),
+        (2, "definicion_2_1"),
+        (3, "definicion_1_1");
+
 END TRANSACTION;
